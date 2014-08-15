e97c245406e16089376d1bfa2c9a00702cd057a9
Jacob Brooks <jacob@chrisbrooks.org>
2014-08-15 12:36:25 -0700

    initial commit

---
+++ versity.tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+using FluentAssertions;
+using NUnit.Framework;
+using versity.Controllers;
+
+namespace versity.tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _homeController = new HomeController();
+        }
+
+        [Test]
+        public void ShouldReturnView()
+        {
+            whenIndexIsCalled();
+            _thenResult.Should().BeOfType<ViewResult>();
+        }
+
+        private void whenIndexIsCalled()
+        {
+            _thenResult = _homeController.Index();
+        }
+
+        private HomeController _homeController;
+        private ActionResult _thenResult;
+    }
+}
---
+++ versity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="versity.MvcApplication" Language="C#" %>

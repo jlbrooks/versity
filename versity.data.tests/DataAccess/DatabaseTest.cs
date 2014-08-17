95465bfe2ffedde615669efc70b0bc0ec2142e72
Jacob Brooks <jacob@chrisbrooks.org>
2014-08-16 21:58:19 -0700

    Restaurant store tests and dbtest framework

---
+++ versity.data.tests/DataAccess/DatabaseTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using versity.data.DataAccess.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using NUnit.Framework;
+
+namespace versity.data.tests.DataAccess
+{
+    public abstract class DatabaseTest
+    {
+        public virtual void SetupTransaction()
+        {
+            _scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted });
+        }
+
+        public virtual void CloseTransaction()
+        {
+            _scope.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected VersityDbContext TestDbContext
+        {
+            get { return Context as VersityDbContext; }
+        }
+
+        [SetUp]
+        protected virtual void setup() 
+        {
+            CreateDbContext();
+            SetupTransaction();
+        }
+
+        [TearDown]
+        protected virtual void teardown()
+        {
+            CloseTransaction();
+            Dispose();
+        }
+
+        protected virtual void CreateDbContext() 
+        {
+            Context = new VersityDbContext();
+        }
+
+        protected void SaveAndValidate()
+        {
+            try
+            {
+                Context.SaveChanges();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposing)
+                return;
+            if (Context == null)
+                return;
+            Context.Dispose();
+            Context = null;
+        }
+
+        protected DbContext Context;
+        private TransactionScope _scope;
+    }
+}
---
+++ versity.data.tests/DataAccess/RestaurantStoreTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using versity.data.DataAccess;
+using versity.data.DataAccess.EntityFramework;
+using versity.data.Models;
+using NUnit.Framework;
+using NSubstitute;
+using FluentAssertions;
+
+namespace versity.data.tests.DataAccess
+{
+    [TestFixture]
+    public class RestaurantStoreTests : DatabaseTest
+    {
+        [SetUp]
+        protected override void setup()
+        {
+            base.setup();
+            _restaurantStore = new RestaurantStore(TestDbContext);
+            ThenRestaurant = null;
+        }
+
+        [Test]
+        public void ShouldAddAndUpdateRestaurant()
+        {
+            var restaurant = new Restaurant { Name = "bar", ID = 2 };
+            WhenAdd(restaurant);
+            WhenGet(restaurant.ID);
+            ThenRestaurant.Name.Should().Be("bar");
+            restaurant.Name = "other";
+            WhenUpdate(restaurant);
+            WhenGet(restaurant.ID);
+            ThenRestaurant.Name.Should().Be("other");
+        }
+
+        [Test]
+        public void ShouldFindByRestaurantID()
+        {
+            GivenRestaurantInContext(SomeRestaurant);
+            WhenGet(SomeRestaurant.ID);
+            ThenRestaurant.Name.Should().Be("foo");
+        }
+
+        [Test]
+        public void ShouldReturnNullForNotFoundRestaurant()
+        {
+            GivenRestaurantInContext(SomeRestaurant);
+            WhenGet(5);
+            ThenRestaurant.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldRemoveRestaurant()
+        {
+            GivenRestaurantInContext(SomeRestaurant);
+            WhenRemove(SomeRestaurant.ID);
+            WhenGet(SomeRestaurant.ID);
+            ThenRestaurant.Should().BeNull();
+        }
+
+        private void GivenRestaurantInContext(Restaurant restaurant)
+        {
+            TestDbContext.Restaurants.Add(restaurant);
+            SaveAndValidate();
+        }
+
+        private void WhenRemove(int id)
+        {
+            _restaurantStore.Remove(id);
+        }
+
+        private void WhenAdd(Restaurant restaurant)
+        {
+            _restaurantStore.Add(restaurant);
+        }
+
+        private void WhenGet(int id)
+        {
+            ThenRestaurant = _restaurantStore.GetByRestaurantID(id);
+        }
+
+        private void WhenUpdate(Restaurant restaurant)
+        {
+            _restaurantStore.Update(restaurant);
+        }
+
+
+        private Restaurant ThenRestaurant { get; set; }
+        private IRestaurantStore _restaurantStore;
+        private static readonly Restaurant SomeRestaurant = new Restaurant
+        {
+            ID = 1,
+            Name = "foo",
+            PhoneNumber = "123",
+            Address = "111 bar st"
+        };
+    }
+}

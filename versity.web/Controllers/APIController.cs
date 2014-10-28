878461778ae9f53a885b43bafade3c1b094f3082
Jacob Brooks <jacob@chrisbrooks.org>
2014-10-28 12:14:58 -0400

    Restaurants under budget now matches restaurant model spec

--- versity.web/Controllers/APIController.cs
+++ versity.web/Controllers/APIController.cs
@@ -25,24 +25,57 @@ namespace versity.Controllers
             return new JsonResult { Data = translated, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
-        // Returns a list of restaurants, with a list of items the fulfill the budget
+        // Returns a list of restaurants, all of which have >= 1 item that is under budget
         public JsonResult GetRestaurantsWithBudget(decimal budget)
         {
             var restaurants = _restaurantStore.HasItemUnderBudget(budget);
-            var translated = restaurants.Select(x => new
-            {
-                ID = x.ID,
-                Name = x.Name,
-                Items = x.Menus.First().Items.Where(i => i.Cost <= budget).Select(i => mapItem(i)),
-                Lat = x.Locations.First().lat,
-                Lng = x.Locations.First().lng
-            });
+            var translated = restaurants.Select(r => mapRestaurant(r));
 
             return new JsonResult
+
             {
                 Data = translated,
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
+        }
+
+        private object mapRestaurant(Restaurant r)
+        {
+            var obj = new
+            {
+                ID = r.ID,
+                Name = r.Name,
+                PhoneNumber = r.PhoneNumber,
+                Locations = r.Locations.Select(l => mapLocation(l)),
+                Menus = r.Menus.Select(m => mapMenu(m))
+            };
+            return obj;
+        }
+
+        private object mapLocation(Location l)
+        {
+            var obj = new
+            {
+                ID = l.ID,
+                Address = l.Address,
+                City = l.City,
+                State = l.State,
+                Lat = l.lat,
+                Lng = l.lng
+            };
+            return obj;
+        }
+
+        private object mapMenu(Menu m)
+        {
+            var obj = new
+            {
+                ID = m.ID,
+                Name = m.Name,
+                Active = m.Active,
+                Items = m.Items.Select(i => mapItem(i))
+            };
+            return obj;
         }
 
         private object mapItem(Item i)
